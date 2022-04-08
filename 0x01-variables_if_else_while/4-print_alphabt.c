a355a6ae667d2989baac5cf1fabfb8397e63fc75
rachelle25 <rachelleavory@gmail.com>
2022-04-08 13:05:29 -0700

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Prints some letters of alphabets
+*
+* Return: Always (success)
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+}
+putchar
+return(0);
+}
