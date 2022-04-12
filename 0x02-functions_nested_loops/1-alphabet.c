cbdd2484fcdc3b9d04c1099102cb05fc13a1ca5c
rachelle25 <rachelleavory@gmail.com>
2022-04-12 11:22:21 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print-alphabet
+* in lowercase, followed by a new line
+*
+*Return: zero (success)
+*/
+
+void print-alphabet(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(chh);
+}
+_putchar('\n');
+
+}
