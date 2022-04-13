94831962d9c14142f1138d80909ab753f894901a
rachelle25 <rachelleavory@gmail.com>
2022-04-13 10:04:37 -0700

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,34 +8,18 @@
 */
 void print_to_98(int n)
 {
-if (n < 98)
+while (n < 98)
 {
-while ( n <= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-printf(",");
-}
+printf("%i,", n);
 n++;
 }
-}
-else if (n > 98)
+while (n > 98)
 {
-while (n >= 98)
-{
-printf("%d", n);
-if (n != 98)
-{
-printf(",");
-}
+printf("%i," n);
 n--;
 }
-}
-else
-{
 printf("98");
+putchar('\n');
 }
-printf("\n");
-}
+
 
