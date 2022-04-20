3eefe653fd1643f4f2af804fd044512d63129332
rachelle25 <rachelleavory@gmail.com>
2022-04-20 13:41:56 -0700

    six

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,24 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
 /**
 * puts2 - prints a string,followed by a new line,
-* @str: pointer to the string to print
+* @str: input
 * Return: void
 */
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
+int i ;
+for (i = 0; str[i] != '\0'; i++)
 {
 if (i % 2 == 0)
 {
- _putchar(str[i]);
+ putchar(str[i]);
 }
-i++;
+}
+putchar('\n');
+
 }
 _putchar('\n');
 }
