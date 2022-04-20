4a9b69efca6e6563d9c11b4d9bc00e604a0c4b94
rachelle25 <rachelleavory@gmail.com>
2022-04-20 07:02:56 -0700

    eight

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 {
 int i;
 i = 0;
-for (n--; n >= 0; n--; i++)
+for (n--; n >= 0; n--, i++)
 {
 printf("%d", a[i]);
 if (n > 0)
 {
-printf(",");
+	printf(",");
 }
 printf("\n");
 }
