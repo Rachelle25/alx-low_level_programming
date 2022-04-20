26b10fd8849e3c22aba57548ab7fc47b712a9305
rachelle25 <rachelleavory@gmail.com>
2022-04-20 14:51:34 -0700

    ten

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,13 +16,13 @@ if (s[i] == '-')
 sig = sig * -1;
 if (s[i] >= '0' && s[i] <= '9')
 {
-res =res * 10;
+res = res * 10;
 res = res - (s[i] - '0');
 brk = 1;
 }
 else if (brk == 1)
 break;
 }
-res = sig * res;
+res = sig *res;
 return (res);
 }
