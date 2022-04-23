9196dd75e755500862302cc3791d0f16102e1f65
rachelle25 <rachelleavory@gmail.com>
2022-04-23 11:42:31 -0700

    one

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,7 +9,7 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
-i =0;
+i = 0;
 while (dest[i] != '\0')
 {
 i++;
@@ -18,7 +18,7 @@ j = 0;
 while (j < n && src[j] != '\0')
 {
 dest[i] = src[j];
-j++
+j++;
 i++;
 }
 dest[i] = '\0';
