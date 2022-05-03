13fea0ff3e232d64d10241888f03a19ba74b8b07
rachelle25 <rachelleavory@gmail.com>
2022-05-03 04:52:28 -0700

    new cfiles

---
+++ 0x09-static_libraries/three.c
@@ -0,0 +1,15 @@
+#include "main.h"
+void _puts(char *s) {
+}
+int _atoi(char *s) {
+return 0;
+}
+char* _strcpy(char* dest, char* src) {
+return "Hello";
+}
+char * _strcat(char* dest, char *src) {
+return "Hello";
+}
+char *_strcat(char *dest, char *src, int n) {
+return "Hello";
+}
---
+++ 0x09-static_libraries/two.c
@@ -0,0 +1,10 @@
+#include "main.h"
+int _isupper(int c) {
+return 0;
+}
+int _isdigit(int c) {
+return 0;
+}
+int _strlen(char *s) {
+return 0;
+}
