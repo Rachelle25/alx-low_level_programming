5c303e0ec96af56d2ccc29d546c88641305e6b22
rachelle25 <rachelleavory@gmail.com>
2022-05-04 02:13:22 -0700

    error

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints the function
+* @argc: counts arguments passed to the function for innt
+* @argv: argument vector
+* Return: 0
+**/
+int main(int argc, char *argv[])
+{
+int i, res = 1;
+if (argc != 3)
+{
+printf("%s\n", "Error");
+return (1);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+res *= atoi(argv[i]);
+}
+printf("%d\n", res);
+}
+return (0);
+}
