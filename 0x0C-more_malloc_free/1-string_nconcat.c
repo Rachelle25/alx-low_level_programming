b893e63d2f9e63517e6c2e7d752add3413b91b03
rachelle25 <rachelleavory@gmail.com>
2022-05-10 03:50:16 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *p;
 unsigned int size1 = 0, size2 = 0, i;
 if (s1 == NULL)
-s1 == "";
+s1 == 0;
 if (s2 == NULL)
-s2 == "";
+s2 == 0;
 while (s1[size1] != '\0')
 {
 size1++;
