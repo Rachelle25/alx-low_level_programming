6ae10e7813904e1778b822356518c293de7d5886
rachelle25 <rachelleavory@gmail.com>
2022-05-27 04:38:43 -0700

    de

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,10 +2,10 @@
 /**
 * listint_len - returns the num of elements
 * @h: linked list of type listint_t to transverse
-* 
+*
 * Return: number of nodes
 */
-size_t listint_len(const listint_t *h))
+size_t listint_len(const listint_t *h)
 {
 size_t num = 0;
 while (h)
