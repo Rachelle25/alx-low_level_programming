c1f62abdd0490a4877cc4efdd347116d69ad05e8
rachelle25 <rachelleavory@gmail.com>
2022-06-01 09:41:15 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,8 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int file, fwrite, i;
-int (filename == NULL)
+
+if (filename == NULL)
 return (-1);
 file = open(filename, O_APPEND | O_RDWR, 0664);
 if (file == -1)
