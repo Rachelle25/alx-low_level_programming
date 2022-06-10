b0eda0ca2bf835b3da211163e54a7465d20c3a51
rachelle25 <rachelleavory@gmail.com>
2022-06-10 07:20:01 -0700

    len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,15 @@
 #include "lists.h"
 /**
-* print_listint - prints all elements of a list
+* listint_len - returns all elements of a list
 * @h: head of a list
 *
 * Return: number of nnodes.
 */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 size_t nnodes = 0;
 while (h != NULL)
 {
-printf("%d\n", h->n);
 h = h->next;
 nnodes++;
 }
