aac7c06489d67dd5f5838c79cb150eecc8a480c5
rachelle25 <rachelleavory@gmail.com>
2022-08-05 03:20:22 -0700

    hash

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,19 +1,11 @@
 #include "hash_tables.h"
-
 /**
-
  *hash_table_get - function that retrieves a value associated with a key
-
  *
-
  *@ht: hash table you want to look into
-
  *@key: the key you are looking for
-
  *
-
  *Return: the value associated with the element else NULL
-
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -23,7 +15,6 @@ if (key == NULL || ht == NULL)
 return (NULL);
 index = key_index((const unsigned char *)key, ht->size);
 nodo = ht->array[index];
-
 for (; nodo != NULL; nodo = nodo->next)
 {
 if (strcmp(nodo->key, key) == 0)
@@ -31,4 +22,3 @@ return (nodo->value);
 }
 return (NULL);
 }
-
